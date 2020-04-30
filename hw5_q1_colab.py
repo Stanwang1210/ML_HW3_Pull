9f8d2471dc79606864df0cac22f64fcc10d141f3
Stanwang1210 <55189574+Stanwang1210@users.noreply.github.com>
2020-04-30 10:36:07 +0800

    Update hw5_q1_colab.py

--- hw5_q1_colab.py
+++ hw5_q1_colab.py
@@ -461,8 +461,8 @@ for key in dictionary.keys():
     #plt.savefig(f'{key}.png')
     np.random.seed(16)                                                                                                                                                       
     # 讓實驗 reproducible
-    for img in images:
-        axs.imshow(img)
+    for i in axs:
+        axs[i].imshow(images, interpolation='none')
     plt.savefig(f'{key}.png')
     
     fig, axs = plt.subplots(1, len(img_indices), figsize=(15, 8))
